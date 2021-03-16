201f16c3938446942b9e76f41ddf9d592c03d778
MicaelCometa <micael@cometa.app>
2021-03-16 16:20:40 +0000

    comit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,55 +17,82 @@ export class HomePage {
   redTeamPoints: number;
   team: any;
   btnColor: string;
-
+ 
   constructor(
     private http: HttpClient,
-    private storage:Storage
+    private storage: Storage
   ) {
-    this.team=false
-    this.btnColor=''
+    this.team = false
+    this.btnColor = ''
     this.storage.get('team').then(
-      r=>{
-        if(r!='red' && r!='blue'){
-          r = (Math.floor(Math.random() * 2) + 1)  == 1 ? 'red':'blue'
-          this.storage.set('team',r)
+      r => {
+        if (r != 'red' && r != 'blue') {
+          r = (Math.floor(Math.random() * 2) + 1) == 1 ? 'red' : 'blue'
+          this.storage.set('team', r)
         }
         this.team = r
-        this.btnColor = r=='red'?'danger':'primary'
+        this.btnColor = r == 'red' ? 'danger' : 'primary'
       }
     )
 
-    this.blueTeamPoints=0
-    this.redTeamPoints=0
-    this.redWidth="60%"
-    this.blueWidth="40%"
+    this.blueTeamPoints = 0
+    this.redTeamPoints = 0
+    this.redWidth = "50%"
+    this.blueWidth = "50%"
     this.pusher = new Pusher('9e682d6958fc4c8493ee', {
       cluster: "eu",
     });
-    
-    var channel = this.pusher.subscribe("cometa")   
-    channel.bind("game", (data) => {
-      // Recibir {red:100,blue:50}
-      console.log('data>>',data)
-      this.calculatePercent(data)
-    }) 
+
+    var channel = this.pusher.subscribe("cometa")
+    channel.bind("game", (r) => {
+      var data = r.results
+      this.prepareStats(data)
+    })
 
   }
 
   ionViewWillEnter() {
-    this.http.post("domain/endpoint/", { observe: "response" }).subscribe(// TODO recibo {red:100,blue:50}
+    this.http.get("https://talk.cometa.app/game/stats", { observe: "response" }).subscribe(
       r => {
-        console.log('r es',r)
-        this.calculatePercent(r)
+        var data: any = r.body
+        this.prepareStats(data)
       }
     )
   }
 
-  calculatePercent(data){
-    var total = data.red+data.blue
-    var redPercent = 100*data.red / total
-    var bluePercent = 100-redPercent
-    this.redWidth = redPercent+'%'
-    this.blueWidth = bluePercent+'%'
+  sendPoint() {
+    var data = {
+      color: this.team == 'red' ? 'rojo' : 'azul'
+    }
+    this.http.post("https://talk.cometa.app/game/add", data, { observe: "response" }).subscribe(
+      r => {
+        var data: any = r.body
+        this.prepareStats(data)
+      })
+  }
+
+  prepareStats(data) {
+    var stats = {
+      red: 0,
+      blue: 0
+    }
+    if (data[0].color == 'rojo') {
+      stats.red = parseInt(data[0].value)
+      stats.blue = parseInt(data[1].value)
+    } else {
+      stats.red = parseInt(data[2].value)
+      stats.blue = parseInt(data[0].value)
+    }
+    this.calculatePercent(stats)
+  }
+
+  calculatePercent(data) {
+    var total = data.red + data.blue
+    this.redTeamPoints = data.red
+    this.blueTeamPoints = data.blue
+    var redPercent = 100 * data.red / total
+    var bluePercent = 100 - redPercent
+    this.redWidth = redPercent + '%'
+    this.blueWidth = bluePercent + '%'
   }
 }
